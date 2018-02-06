f43efbab90284ce914f96b6c0de31025f187a89c
EmilioLdelaFuente <emilio.ldlf@gmail.com>
2018-02-06 16:26:03 +0100

    Initial Commit
    nonfunctional simulink model + initialization script

---
+++ InitSteady.m
@@ -0,0 +1,16 @@
+%Codigo para inicializar las variables de la steady cam
+
+
+%% Variables de la configuracion
+
+Ts=0.03;
+off=0;
+
+%% Variables del modelo
+
+InitTime=2.9;
+GainGyro=90/12000;
+Rad2Deg=180/pi;
+PID_Activator=35;
+
+%% 
